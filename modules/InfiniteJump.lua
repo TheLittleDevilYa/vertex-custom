a12256a42b4c3d7bbf69afff0071ba9395502cbd
TheLittleDevilYa <147209465+TheLittleDevilYa@users.noreply.github.com>
2025-05-16 05:10:38 -0300

    InfiniteJump.lua

--- modules/InfiniteJump.lua
+++ modules/InfiniteJump.lua
@@ -1,1 +1,17 @@
+local InfiniteJump = {}
+InfiniteJump.Enabled = false
 
+local UserInputService = game:GetService("UserInputService")
+local player = game.Players.LocalPlayer
+
+function InfiniteJump:SetEnabled(value)
+    self.Enabled = value
+end
+
+UserInputService.JumpRequest:Connect(function()
+    if InfiniteJump.Enabled and player.Character and player.Character:FindFirstChild("Humanoid") then
+        player.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+    end
+end)
+
+return InfiniteJump
