d35efecd9ebabfa242133d3eed1df40172c87a14
TheLittleDevilYa <147209465+TheLittleDevilYa@users.noreply.github.com>
2025-05-16 05:10:21 -0300

    NoClip.lua

--- modules/NoClip.lua
+++ modules/NoClip.lua
@@ -1,1 +1,42 @@
+local NoClip = {}
+NoClip.Enabled = false
 
+local RunService = game:GetService("RunService")
+local player = game.Players.LocalPlayer
+
+function NoClip:SetEnabled(value)
+    self.Enabled = value
+    if value then
+        self:Enable()
+    else
+        self:Disable()
+    end
+end
+
+function NoClip:Enable()
+    self.Connection = RunService.Stepped:Connect(function()
+        if player.Character and player.Character:FindFirstChild("Humanoid") then
+            for _, part in pairs(player.Character:GetChildren()) do
+                if part:IsA("BasePart") and part.CanCollide then
+                    part.CanCollide = false
+                end
+            end
+        end
+    end)
+end
+
+function NoClip:Disable()
+    if self.Connection then
+        self.Connection:Disconnect()
+        self.Connection = nil
+    end
+    if player.Character then
+        for _, part in pairs(player.Character:GetChildren()) do
+            if part:IsA("BasePart") then
+                part.CanCollide = true
+            end
+        end
+    end
+end
+
+return NoClip
