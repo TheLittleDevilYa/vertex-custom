cb5e544002235d75f885c28e8a2e7c01b6cc3835
TheLittleDevilYa <147209465+TheLittleDevilYa@users.noreply.github.com>
2025-05-16 05:10:49 -0300

    AntiFling.lua

--- modules/AntiFling.lua
+++ modules/AntiFling.lua
@@ -1,1 +1,32 @@
+local AntiFling = {}
+AntiFling.Enabled = false
 
+local RunService = game:GetService("RunService")
+local player = game.Players.LocalPlayer
+
+function AntiFling:SetEnabled(value)
+    self.Enabled = value
+    if value then
+        self:Start()
+    else
+        self:Stop()
+    end
+end
+
+function AntiFling:Start()
+    self.Connection = RunService.Heartbeat:Connect(function()
+        if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+            player.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+            player.Character.HumanoidRootPart.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
+        end
+    end)
+end
+
+function AntiFling:Stop()
+    if self.Connection then
+        self.Connection:Disconnect()
+        self.Connection = nil
+    end
+end
+
+return AntiFling
