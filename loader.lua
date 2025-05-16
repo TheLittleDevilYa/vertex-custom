2d3015a523eceb94a3be40c116c664addf7ea0f5
TheLittleDevilYa <147209465+TheLittleDevilYa@users.noreply.github.com>
2025-05-16 05:42:50 -0300

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/TheLittleDevilYa/vertex-custom/main/loader.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheLittleDevilYa/vertex-custom/main/main.lua"))()
